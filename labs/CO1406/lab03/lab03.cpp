e92efdf713d16270eec8a05cddc2dddb61bb50df
Panayiotis Andreou <pgandreou@Panics-MacBook-Pro.local>
2020-02-03 01:46:50 +0200

    Update lab03.cpp

--- labs/CO1406/lab03/lab03.cpp
+++ labs/CO1406/lab03/lab03.cpp
@@ -33,86 +33,82 @@ void updateMovie(movieDatabase& db, string key, string title, unsigned short yea
 void findMoviesByYear(movieDatabase& db, unsigned short year);
 void deleteMovie(movieDatabase& db, string key);
 void sortMovies(movieDatabase& db);
+int compare(const void* a, const void* b);
 
 int main()
 {
     movieDatabase db;
-    
+
     string key;
     string title;
     unsigned short year;
     unsigned short rating;
 
     int choice = -1;
-    while ( (choice = menu()) != 10) {
+    while ((choice = menu()) != 10) {
         switch (choice) {
-            case 1: 
-                showMovies(db);
-                break;
-            
-            case 2: 
-                cout << "Enter movie title to search: ";
-                cin >> key;
-                cout << "Position of " << key << findMovie(db, key) << endl;
-                break;
-            
-            case 3: 
-                cout << "Enter new movie data {title year rating}: ";
-                cin >> title >> year >> rating;
-                addMovie(db, title, year, rating);
-                break;
-            
-            case 4:
-                cout << "Enter movie to delete: ";
-                cin >> key;
-                deleteMovie(db, key);
-                break;
-            
-            case 5:
-                cout << "Enter movie to update: ";
-                cin >> key;
-                cout << "Enter new movie data {title year rating}: ";
-                cin >> title >> year >> rating;
-                updateMovie(db, key, title, year, rating);
-                break;
-            
-            case 6: 
-                cout << printMovie(db.movies[findMovieMaxRating(db)]);
-                break;
-            
-            case 7:
-                cout << "Enter year to search: ";
-                cin >> year;
-                findMoviesByYear(db, year);
-                break;
-            
-            case 8:
-                sortMovies(db);
-                break;
-            
-            case 9: 
-                loadDatabase(db);
-                break;
-            
-            default: 
-                cout << "\n\n!!!Invalid Choice!!!\n\n" << endl;
+        case 1:
+            showMovies(db);
+            break;
+
+        case 2:
+            cout << "Enter movie title to search: ";
+            cin >> key;
+            cout << "Position of " << key << findMovie(db, key) << endl;
+            break;
+
+        case 3:
+            cout << "Enter new movie data {title year rating}: ";
+            cin >> title >> year >> rating;
+            addMovie(db, title, year, rating);
+            break;
+
+        case 4:
+            cout << "Enter movie to delete: ";
+            cin >> key;
+            deleteMovie(db, key);
+            break;
+
+        case 5:
+            cout << "Enter movie to update: ";
+            cin >> key;
+            cout << "Enter new movie data {title year rating}: ";
+            cin >> title >> year >> rating;
+            updateMovie(db, key, title, year, rating);
+            break;
+
+        case 6:
+            cout << printMovie(db.movies[findMovieMaxRating(db)]);
+            break;
+
+        case 7:
+            cout << "Enter year to search: ";
+            cin >> year;
+            findMoviesByYear(db, year);
+            break;
+
+        case 8:
+            sortMovies(db);
+            break;
+
+        case 9:
+            loadDatabase(db);
+            break;
+
+        default:
+            cout << "\n\n!!!Invalid Choice!!!\n\n" << endl;
         }
         fflush(NULL);
     }
 }
 
-int compare(const void* a, const void* b)
-{
-    return (*(movie*)a).rating - (*(movie*)b).rating;
-}
-
- int menu() {
+int menu() {
     cout << "+------------------------+" << endl;
     cout << "|        Main Menu       |" << endl;
     cout << "+------------------------+" << endl;
     cout << endl << endl;
 
-    cout << "1. Show List of Movies" << endl; 
+    cout << "1. Show List of Movies" << endl;
     cout << "2. Search for a Movie" << endl;
     cout << "3. Add a Movie" << endl;
     cout << "4. Delete a Movie" << endl;
@@ -121,9 +117,9 @@ int compare(const void* a, const void* b)
     cout << "7. Find movies of year" << endl;
     cout << "8. Sort movies by rating" << endl;
     cout << "9. Load Movies from File" << endl;
-    cout << "10. Exit" << endl<<endl;
+    cout << "10. Exit" << endl << endl;
 
-    cout << "Enter choice: " ;
+    cout << "Enter choice: ";
 
     static int choice = -1;
     cin >> choice;
@@ -136,9 +132,12 @@ string printMovie(movie m) {
 }
 
 void showMovies(movieDatabase& db) {
+    //check if the database is empty
+    //if it is, print an empty message
     if (db.count == 0) {
         cout << "Database empty!" << endl;
     }
+    //if not, print all the movies {Title tab Year tab Rating), one in every line
     else {
         cout << "+------------------------+" << endl;
         cout << "|     Movie Database     |" << endl;
@@ -153,20 +152,65 @@ void showMovies(movieDatabase& db) {
 }
 
 void addMovie(movieDatabase& db, string title, unsigned short year, unsigned short rating) {
-    db.movies[db.count].title = title;
-    db.movies[db.count].year = year;
-    db.movies[db.count].rating = rating;
-    db.count++;
+    //check if the database is full
+    //if it is not, add the movie
+    if (db.count < MAX) {
+        db.movies[db.count].title = title;
+        db.movies[db.count].year = year;
+        db.movies[db.count].rating = rating;
+        db.count++;
+    }
+    //else print that the Database is full
+    else cout << "Database is full!" << endl;
+}
+
+void loadDatabase(movieDatabase& db) {
+    ifstream file("movies.csv");
+    //declare appropriate string variables to store the strings read from the file
+    string title;
+    string yearString;
+    string ratingString;
+
+    //declare appropriate short variables to convert year and rating strings
+    unsigned short year;
+    unsigned short rating;
+
+    //reset the database counter
+    db.count = 0;
+    //read and skip the first line
+    getline(file, title);
+
+    while (file.good())
+    {
+        //read the title until the first comma
+        getline(file, title, ',');
+        //read the year until the second comma
+        getline(file, yearString, ',');
+        //read the rating until the end of the line
+        getline(file, ratingString, '\n');
+
+        //convert year and rating
+        year = stoi(yearString);
+        rating = stoi(ratingString);
+
+        //call addMovie to add the movie read into the database
+        addMovie(db, title, year, rating);
+    }
+    cout << endl << "File loaded succesfully" << endl;
 }
 
 unsigned int findMovie(movieDatabase& db, string key) {
-    for(int i=0; i<db.count; i++)
+    //search for a movie with a title equal to the key
+    //if one is found, return the key
+    //otherwise, return -1
+    for (int i = 0; i < db.count; i++)
         if (db.movies[i].title == key) return i;
-     
+
     return -1;
 }
 
 unsigned int findMovieMaxRating(movieDatabase& db) {
+    //search for the movie with a the maximum rating and return its position
     unsigned short maxRatingIndex = 0;
 
     for (int i = 1; i < db.count; i++)
@@ -176,66 +220,54 @@ unsigned int findMovieMaxRating(movieDatabase& db) {
 }
 
 void findMoviesByYear(movieDatabase& db, unsigned short year) {
+    //search for all movies that were released in a specific year
     for (int i = 0; i < db.count; i++)
         if (db.movies[i].year == year)
             cout << printMovie(db.movies[i]);
 }
 
-void updateMovie(movieDatabase& db, string key, string title, unsigned short year, unsigned short rating) {
+void updateMovie(movieDatabase& db, string key, string newTitle, unsigned short newYear, unsigned short newRating) {
+    //search for a movie with a title equal to the key using findMovie
+    //if one is found, update its title, year and rating with the new data   
     unsigned int pos = -1;
     pos = findMovie(db, key);
     if (pos != -1) {
-        db.movies[pos].title = title;
-        db.movies[pos].year = year;
-        db.movies[pos].rating = rating;
+        db.movies[pos].title = newTitle;
+        db.movies[pos].year = newYear;
+        db.movies[pos].rating = newRating;
     }
+    //otherwise, print that the selected movie was not found
     else {
         cout << "Movie " << key << " not found!" << endl;
     }
-        
+
 }
 
 void deleteMovie(movieDatabase& db, string key) {
     unsigned int pos = -1;
+    //find the position of the element you are trying to delete using findMovie
     pos = findMovie(db, key);
-    if (pos != -1) {
+    //check if the position is found
+    //if it is not found then print that the specific movie is not found (include the name of the movie)
+    if (pos == -1) {
+        cout << "Movie " << key << " not found!" << endl;
+    }
+    //if it is found then delete the movie from the database
+    //it is important that the movie slot does not remain empty
+    else {
         for (int i = pos; i < db.count; i++) db.movies[i] = db.movies[i + 1];
         db.count--;
     }
-    else {
-        cout << "Movie " << key << " not found!" << endl;
-    }
 
+}
+
+int compare(const void* a, const void* b)
+{
+    return (*(movie*)a).rating - (*(movie*)b).rating;
 }
 
 void sortMovies(movieDatabase& db) {
+    //use quicksort (qsort) to sort the movie database
     qsort(db.movies, db.count, sizeof(movie), compare);
 }
 
-void loadDatabase(movieDatabase& db) {
-    ifstream file("movies.csv"); 
-    string title;
-    string yearString;
-    string ratingString;
-
-    unsigned short year;
-    unsigned char rating;
-    
-    //reset the database
-    db.count = 0;
-    //skip the first line
-    getline(file, title);
-
-    while (file.good())
-    {
-        getline(file, title, ','); 
-        getline(file, yearString, ',');
-        getline(file, ratingString, '\n');
-
-        year = stoi(yearString);
-        rating = stoi(ratingString);
-
-        addMovie(db, title, year, rating); 
-    }
-    cout << endl << "File loaded succesfully" << endl;
-}
